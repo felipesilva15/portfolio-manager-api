26b021b878de9065f577ce73627fb8613929a11f
Felipe Silva <felipe.allware@gmail.com>
2025-02-26 19:30:51 -0300

    feat: Criando tabelas básicas

---
+++ database/migrations/2025_02_26_221449_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->string('description', 4096);
+            $table->date('completion_date')->nullable();
+            $table->string('thumbnail_url', 255);
+            $table->enum('status', ['PENDING', 'IN_PROGRESS', 'COMPLETED', 'ABANDONED']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
