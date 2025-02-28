79d37175126b9fe9ab63b360a7e59502b924b5c1
felipe.silva <felipe.silva@pwi.com.br>
2025-02-28 13:44:24 -0300

    feat: Criando recursos para tags

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('project', ProjectController::class);
+Route::apiResource('tag', TagController::class);
