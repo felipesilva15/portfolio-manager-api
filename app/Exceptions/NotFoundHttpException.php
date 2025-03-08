1e9b67561db40b273c642fc034814f5c60568511
felipe.silva <felipe.silva@pwi.com.br>
2025-03-07 21:02:25 -0300

    fix: Ajuste em notfound exception

--- app/Exceptions/NotFoundHttpException.php
+++ app/Exceptions/NotFoundHttpException.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class NotFoundHttpException extends HttpException
 {
-    public function __construct(string $message = 'Registro não encontrado.', \Throwable $previous = null, int $code = 0, array $headers = []) {
+    public function __construct(string $message = 'Registro não encontrado.', \Throwable $previous = null, int $code = 404, array $headers = []) {
         parent::__construct(404, $message, $previous, $headers, $code);
     }
 
