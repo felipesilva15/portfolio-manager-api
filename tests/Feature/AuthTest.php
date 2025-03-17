96305cf5cf265f78a273c6a98fb75dd4a9bf48ee
felipe.silva <felipe.silva@pwi.com.br>
2025-03-17 20:29:00 -0300

    test: Criando testes para os recursos de autenticação

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function test_can_login(): void
+    {
+        $user = User::factory()->createOne();
+        $data = [
+            'email' => $user->email,
+            'password' => "123",
+        ];
+
+        $response = $this->postJson('/api/login', $data);
+
+        $response->assertStatus(200)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'access_token',
+                    'token_type',
+                    'expires_in'
+                ]);
+    }
+
+    public function test_cannot_login_with_invalid_credentials(): void
+    {
+        $user = User::factory()->createOne();
+        $data = [
+            'email' => $user->email,
+            'password' => "123456",
+        ];
+
+        $response = $this->postJson('/api/login', $data);
+
+        $response->assertStatus(401)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'path',
+                    'code',
+                    'message'
+                ]);
+    }
+
+    public function test_can_logout(): void
+    {
+        $response = $this->postJson('/api/logout', [], $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'message'
+                ]);
+    }
+
+    public function test_can_refresh_token(): void
+    {
+        $response = $this->postJson('/api/refresh-token', [], $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'access_token',
+                    'token_type',
+                    'expires_in'
+                ]);
+    }
+
+    public function test_can_get_data_logged_in_user(): void
+    {
+        $response = $this->getJson('/api/me', $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'id',
+                    'name',
+                    'email',
+                    'updated_at',
+                    'created_at'
+                ])
+                ->assertJsonFragment([
+                    'id' => $this->authUser->id,
+                    'name' => $this->authUser->name,
+                    'email' => $this->authUser->email,
+                ]);
+    }
+}
