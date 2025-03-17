aab4e0d22afdacb2ff8006ffaee225d67d2b203d
felipe.silva <felipe.silva@pwi.com.br>
2025-03-17 20:28:02 -0300

    test: Criando autenticação base para testes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,38 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected string $authToken;
+    protected User $authUser;
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->generateAndSetAuthData();
+    }
+
+    protected function generateAndSetAuthData(): void {
+        $user = User::factory()->create();
+        $credentials = [
+            'email' => $user->email,
+            'password' => "123",
+        ];
+
+        $this->authToken = JWTAuth::attempt($credentials);
+        $this->authUser = $user;
+    }
+
+    protected function getAuthHeaders() : array {
+        return [
+            "Authorization" => "Bearer {$this->authToken}"
+        ];
+    }
 }
