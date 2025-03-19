0d46d489ab2c28984e7cd8ff36de7f7399207e29
Felipe Silva <felipe.allware@gmail.com>
2025-03-19 12:55:26 -0300

    test: Criando testes para Tag

---
+++ tests/Feature/TagTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use Tests\TestCase;
+
+class TagTest extends TestCase
+{
+    public function test_can_list_tags(): void
+    {
+        Tag::factory(3)->create();
+        $response = $this->getJson('/api/tag', $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonIsArray()
+            ->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'name',
+                    'updated_at',
+                    'created_at'
+                ]
+            ]);
+    }
+
+    public function test_can_get_tag_by_id(): void
+    {
+        $tag = Tag::factory()->createOne();
+
+        $response = $this->getJson('/api/tag/'.$tag->id,  $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['name' => $tag->name]);
+    }
+
+    public function test_cannot_get_tag_by_invalid_id(): void
+    {
+        $response = $this->getJson('/api/tag/999999',  $this->getAuthHeaders());
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+
+    public function test_can_create_tag(): void
+    {
+        $tag = Tag::factory()->makeOne();
+        $data = $tag->toArray();
+
+        $response = $this->postJson('/api/tag/', $data, $this->getAuthHeaders());
+
+        $response->assertStatus(201)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['name' => $tag->name]);
+    }
+
+    public function test_can_update_tag(): void
+    {
+        $tag = Tag::factory()->createOne();
+        $data = $tag->toArray();
+        $data['name'] = 'New name';
+
+        $response = $this->putJson('/api/tag/'.$tag->id, $data,  $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['name' => 'New name']);
+    }
+
+    public function test_cannot_update_tag_with_invalid_id(): void
+    {
+        $tag = Tag::factory()->createOne();
+        
+        $data = $tag->toArray();
+        $data['name'] = 'New name';
+
+        $response = $this->putJson('/api/tag/999999', $data,  $this->getAuthHeaders());
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+
+    public function test_can_delete_tag_by_id(): void
+    {
+        $tag = Tag::factory()->createOne();
+
+        $response = $this->deleteJson('/api/tag/'.$tag->id, [],  $this->getAuthHeaders());
+
+        $response->assertNoContent();
+    }
+
+    public function test_cannot_delete_tag_by_invalid_id(): void
+    {
+        $response = $this->deleteJson('/api/tag/999999', [],  $this->getAuthHeaders());
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+}
