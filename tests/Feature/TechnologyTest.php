f1c918cf0428aec61f6f16cbf5be693182144950
felipe.silva <felipe.silva@pwi.com.br>
2025-05-09 23:27:27 -0300

    test: Criando tests para Technology

---
+++ tests/Feature/TechnologyTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Technology;
+use Tests\TestCase;
+
+class TechnologyTest extends TestCase
+{
+    public function test_can_list_technologys(): void
+    {
+        Technology::factory(3)->create();
+        $response = $this->getJson('/api/technology');
+
+        $response->assertStatus(200)
+            ->assertJsonIsArray()
+            ->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'title',
+                    'icon_url',
+                    'updated_at',
+                    'created_at'
+                ]
+            ])
+            ->assertJsonCount(3);
+    }
+
+    public function test_can_get_technology_by_id(): void
+    {
+        $technology = Technology::factory()->createOne();
+
+        $response = $this->getJson('/api/technology/'.$technology->id);
+
+        $response->assertStatus(200)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'title',
+                'icon_url',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['title' => $technology->title]);
+    }
+
+    public function test_cannot_get_technology_by_invalid_id(): void
+    {
+        $response = $this->getJson('/api/technology/999999');
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+
+    public function test_can_create_technology(): void
+    {
+        $technology = Technology::factory()->makeOne();
+        $data = $technology->toArray();
+
+        $response = $this->postJson('/api/technology/', $data, $this->getAuthHeaders());
+
+        $response->assertStatus(201)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'title',
+                'icon_url',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['title' => $technology->title]);
+    }
+
+    public function test_can_update_technology(): void
+    {
+        $technology = Technology::factory()->createOne();
+        $data = $technology->toArray();
+        $data['title'] = 'New title';
+
+        $response = $this->putJson('/api/technology/'.$technology->id, $data,  $this->getAuthHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonIsObject()
+            ->assertJsonStructure([
+                'id',
+                'title',
+                'icon_url',
+                'updated_at',
+                'created_at'
+            ])
+            ->assertJsonFragment(['title' => 'New title']);
+    }
+
+    public function test_cannot_update_technology_with_invalid_id(): void
+    {
+        $technology = Technology::factory()->createOne();
+        
+        $data = $technology->toArray();
+        $data['title'] = 'New title';
+
+        $response = $this->putJson('/api/technology/999999', $data,  $this->getAuthHeaders());
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+
+    public function test_can_delete_technology_by_id(): void
+    {
+        $technology = Technology::factory()->createOne();
+
+        $response = $this->deleteJson('/api/technology/'.$technology->id, [],  $this->getAuthHeaders());
+
+        $response->assertNoContent();
+    }
+
+    public function test_cannot_delete_technology_by_invalid_id(): void
+    {
+        $response = $this->deleteJson('/api/technology/999999', [],  $this->getAuthHeaders());
+
+        $response->assertNotFound()
+                    ->assertJsonStructure([
+                        'path',
+                        'code',
+                        'message'
+                    ]);
+    }
+}
