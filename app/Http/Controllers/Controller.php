fdfd722f1e90a09539ce27340670826233c1fd78
felipe.silva <felipe.silva@pwi.com.br>
2025-05-23 13:31:53 -0300

    feat: Configuração da base da documentação no Swager

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      title="Portfólio API Docs",
+ *      version="1.0.0",
+ *      description="API para consulta de dados do portfólio",
+ *      @OA\Contact(
+ *          email="felipe.allware@gmail.com",
+ *          name="Felipe Silva"
+ *      ),
+ *      @OA\License(
+ *          name="Licença API",
+ *          url="https://github.com/felipesilva15/portfolio-manager-api/blob/main/LICENSE"
+ *      )
+ * )
+ */
 abstract class Controller
 {
 
