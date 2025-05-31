cc404933083a81bd16c8af5d451a486fd7463339
Felipe Silva <felipe.allware@gmail.com>
2025-05-30 21:19:57 -0300

    feat: Criando entidades para Link

---
+++ database/migrations/2025_05_30_235949_create_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('label', 40);
+            $table->string('url', 512);
+            $table->string('icon_name', 40)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
